51a01a94a67be59dcd8b9dfaed0ef4aa642c17c3
Killian Gascon <gasconkillian.pro@gmail.com>
2024-10-12 19:42:22 +0200

    Commit  1.3 : modifications body

---
+++ portfolio/src/components/Work.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div>
+      <div className="text-left bg-purple-300">
+            <h1 className="text-4xl text-purple-1100 font-bold">Mes technos préférées</h1>
+            <div className='flex items-center'>
+                <div className=''>
+                    
+                    <img src="/react.svg" alt="react" className="h-8 w-8" />
+                    <p className="text-purple-1100 text-lg mt-4"><br/>Python</p>
+                </div>
+                <div className='flex'>
+                    <img src="/react.svg" alt="react" className="h-8 w-8" />
+                    <p className="text-purple-1100 text-lg mt-4"><br/>PHP</p>
+                </div>
+                <div className='flex'>
+                    <img src="/react.svg" alt="react" className="h-8 w-8" />
+                    <p className="text-purple-1100 text-lg mt-4"><br/>Java</p>
+                </div>
+                <div className='flex'>
+                    
+                    <img src="/react.svg" alt="react" className="h-8 w-8" />
+                    <p className="text-purple-1100 text-lg mt-4"><br/>Javascript (surtout React)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
--- portfolio/src/components/body.jsx
+++ portfolio/src/components/body.jsx
@@ -6,8 +6,10 @@ const body = () => {
         <div className="w-full h-screen bg-gradient-to-r from-purple-500 to-purple-300">
             <div className="max-w-7xl mx-auto flex items-center justify-center h-full">
                 <div className="text-center">
-                    <h1 className="text-4xl text-white font-bold">Bonjour, Je suis Killian Gascon</h1>
-                    <p className="text-white text-lg mt-4">Je suis Etudiant en deuxième année d'étude d'informatique !</p>
+                    <h1 className="text-4xl text-purple-1000 font-bold">Bonjour, Je suis Killian Gascon</h1>
+                    <p className="text-purple-1000 text-lg mt-4">
+                        Je suis Etudiant en deuxième année d'étude d'informatique !
+                    </p>
                 </div>
             </div>
         </div>
@@ -15,56 +17,55 @@ const body = () => {
             <div className="flex items-center justify-center h-full">
                 <div className="text-left bg-purple-950">
                     <h1 className="text-4xl text-purple-1100 font-bold">Qui je suis ?</h1>
-                    <p className="text-purple-1100 text-lg mt-4">Bonjour, je m'appelle Killian Gascon, je suis actuellement étudiant en informatique afin de devenir développeur ou expert en cybersécurité</p>
+                    <p className="text-purple-1100 text-lg mt-4">
+                        Bonjour, je m'appelle Killian Gascon, je suis actuellement étudiant en informatique afin de devenir développeur ou expert en cybersécurité
+                    </p>
                 </div>
                 <div className="bg-purple-300">
                     <h1 className="text-4xl text-purple-1100 font-bold">Etudes</h1>
-                    <p className="text-purple-1100 text-lg mt-4">Ecole : Epsi Nantes<br/>Formation : BTS SIO<br/>Diplômes : Permis de conduire, Baccalauréat Général, PSC1 (diplôme de premiers secours)</p>
+                    <p className="text-purple-1100 text-lg mt-4">
+                        Ecole : Epsi Nantes<br/>
+                        Formation : BTS SIO<br/>
+                        Diplômes : Permis de conduire, Baccalauréat Général, PSC1 (diplôme de premiers secours)
+                    </p>
                 </div>
-                <div className="text-left bg-purple-300">
+                <div className="bg-purple-300">
+                    <h1 className="text-4xl text-purple-1100 font-bold">Mes Passes-temps</h1>
+                    <p className="text-purple-1100 text-lg mt-4">
+                    La programmation, le dessin sur tablette 
+                    graphique et papier, la lecture, 
+                    les jeux vidéo, l'Apiculture, le Vélo, 
+                    la musique 
+                    </p>
+                </div>
+                {/* <div className="text-left bg-purple-300">
                     <h1 className="text-4xl text-purple-1100 font-bold">Mes technos préférées</h1>
                     <div className='flex items-center'>
                         <div className=''>
                             {/* Le logo react */}
-                            <img src="/react.svg" alt="react" className="h-8 w-8" />
+                            {/*<img src="/react.svg" alt="react" className="h-8 w-8" />
                             <p className="text-purple-1100 text-lg mt-4"><br/>Python</p>
                         </div>
                         <div className='flex'>
                             {/* Le logo react */}
-                            <img src="/react.svg" alt="react" className="h-8 w-8" />
+                            {/*<img src="/react.svg" alt="react" className="h-8 w-8" />
                             <p className="text-purple-1100 text-lg mt-4"><br/>PHP</p>
                         </div>
                         <div className='flex'>
                             {/* Le logo react */}
-                            <img src="/react.svg" alt="react" className="h-8 w-8" />
+                            {/*<img src="/react.svg" alt="react" className="h-8 w-8" />
                             <p className="text-purple-1100 text-lg mt-4"><br/>Java</p>
                         </div>
                         <div className='flex'>
                             {/* Le logo react */}
-                            <img src="/react.svg" alt="react" className="h-8 w-8" />
+                            {/*<img src="/react.svg" alt="react" className="h-8 w-8" />
                             <p className="text-purple-1100 text-lg mt-4"><br/>Javascript (surtout React)</p>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
-        <div className="bg-purple-300">
-            <h1 className="text-4xl text-purple-1100 font-bold">Expérience Professionnelle</h1>
-            <div>
-                <p className="text-purple-1100 text-lg mt-4">Stage à Thalès: <br/>Stage de première année de BTS SIO. 
-                    Découverte du déroulement d’un projet dans une grande 
-                    entreprise et développement d’un logiciel en python servant à 
-                    piloter des équipements radiofréquence.
-                </p>
-            </div>
-            <div>
-                <p className="text-purple-1100 text-lg mt-4">Travail saisonnier à Leclerc: <br/>
-                    Expérience professionnelle qui 
-                    m’as appris le travail en équipe et la vie d’une entreprise de 
-                    grande distribution.
-                </p>
-            </div>
-        </div>
+        
     </div>
   )
 }
